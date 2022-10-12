7c777919e41f93efacae35b2c60fff9dee2a6688
Raj <rajsaija4@gmail.com>
2022-10-12 14:35:52 +0530

    Base Project

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -65,24 +65,25 @@ struct ContentView: View {
     */
     
     // MARK: - 5. ANIMATION
-    
-    AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
-      switch phase {
-      case .success(let image):
-        image
-          .imageModifier()
-          // .transition(.move(edge: .bottom))
-          // .transition(.slide)
-          .transition(.scale)
-      case .failure(_):
-        Image(systemName: "ant.circle.fill").iconModifier()
-      case .empty:
-        Image(systemName: "photo.circle.fill").iconModifier()
-      @unknown default:
-        ProgressView()
-      }
-    }
-    .padding(40)
+          
+          AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+              switch phase {
+              case .success(let image):
+                  image
+                      .imageModifier()
+                  // .transition(.move(edge: .bottom))
+                  // .transition(.slide)
+                      .transition(.scale)
+              case .failure(_):
+                  Image(systemName: "ant.circle.fill").iconModifier()
+              case .empty:
+                  Image(systemName: "photo.circle.fill").iconModifier()
+              @unknown default:
+                  ProgressView()
+              }
+          }
+          .padding(40)
+
   }
 }
 
