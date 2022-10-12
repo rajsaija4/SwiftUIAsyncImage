316d3471eb91fe124e1edf93518c27e5be270e21
Raj <rajsaija4@gmail.com>
2022-10-12 14:44:42 +0530

    new branch create with second screen

---
+++ SwiftUIAsyncImage/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Raj Saija on 12/10/22.
+//
+
+import SwiftUI
+
+struct SecondViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding(40)
+            .background(.red)
+    }
+}
+
+struct SecondViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondViewController()
+    }
+}
