814071ec395e53322cf784bedecef3d0482f4adb
Raj <rajsaija4@gmail.com>
2022-10-12 14:57:10 +0530

    new branch

---
+++ SwiftUIAsyncImage/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Raj Saija on 12/10/22.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .padding(40)
+            .background(.red)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
